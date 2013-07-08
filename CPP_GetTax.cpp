92772af36563e489b6607e1d4bef9ee85d7cf48b
SeanRustAvalara <sean.rust@avalara.com>
2013-07-08 13:44:13 -0700

    Update CPP_GetTax.cpp

--- CPP_GetTax.cpp
+++ CPP_GetTax.cpp
@@ -19,8 +19,8 @@ int main(array<System::String ^> ^args)
 	GetTaxRequest ^getTaxRequest = gcnew GetTaxRequest();
 	GetTaxResult ^getTaxResult;
 	Address ^documentOrigAddress = gcnew Address();
-    Address ^documentDestAddress = gcnew Address();
-    Line ^lItem = gcnew Line();
+	Address ^documentDestAddress = gcnew Address();
+	Line ^lItem = gcnew Line();
 
 	// Profile->Client is required - pattern is ERPName,majver,minver[,connectorname,majver,minver]
 	taxSvc->Profile->Client = "CPP_GetTax,1,0";
@@ -29,26 +29,26 @@ int main(array<System::String ^> ^args)
 	taxSvc->Configuration->Security->Account = args[1];
 	taxSvc->Configuration->Security->License = args[2];	
 	getTaxRequest->CompanyCode = args[3];
-
-    // Default (header) Origin Address for GetTax
-    documentOrigAddress->Line1 = "100 Ravine Lane NE";
-    documentOrigAddress->Line2 = "Suite 220";
-    documentOrigAddress->City = "Bainbridge Island";
-    documentOrigAddress->Region = "WA";
-    documentOrigAddress->PostalCode = "98110";
-    documentOrigAddress->Country = "US";
+	
+	// Default (header) Origin Address for GetTax
+	documentOrigAddress->Line1 = "100 Ravine Lane NE";
+	documentOrigAddress->Line2 = "Suite 220";
+	documentOrigAddress->City = "Bainbridge Island";
+	documentOrigAddress->Region = "WA";
+	documentOrigAddress->PostalCode = "98110";
+	documentOrigAddress->Country = "US";
 
 	// Default (header) Destination Address for GetTax
 	documentDestAddress->Line1 = "7462 Kearny St";
-    documentDestAddress->Line2 = "";
-    documentDestAddress->City = "Commerce City";
-    documentDestAddress->Region = "CO";
-    documentDestAddress->PostalCode = "80022-1335";
-    documentDestAddress->Country = "US";
-    getTaxRequest->DestinationAddress = documentDestAddress;
-    getTaxRequest->OriginAddress = documentOrigAddress;
+	documentDestAddress->Line2 = "";
+	documentDestAddress->City = "Commerce City";
+	documentDestAddress->Region = "CO";
+	documentDestAddress->PostalCode = "80022-1335";
+	documentDestAddress->Country = "US";
+	getTaxRequest->DestinationAddress = documentDestAddress;
+	getTaxRequest->OriginAddress = documentOrigAddress;
 	
-    /* DetailLevel sets the amount of information returned in the GetTaxResult
+	/* DetailLevel sets the amount of information returned in the GetTaxResult
 	 * these are in order of detail complexity
 	 * Summary		//returns total tax for each jurisdiction without line detail
 	 * Document		//returns tax for complete order
@@ -57,16 +57,15 @@ int main(array<System::String ^> ^args)
 	 * Diagnostic	//ONLY FOR USE ON TEST SYSTEMS - returns all details
 	*/
 	getTaxRequest->DetailLevel = DetailLevel::Tax;
-
-    /* DocType determines whether the document is an order or an invoice
+	
+	/* DocType determines whether the document is an order or an invoice
 	 * Orders are temporary - invoices are permanent (saved)
 	 * SalesOrder, SalesInvoice, PurchaseOrder, PurchaseInvoice, ReturnOrder, ReturnInvoice */
 	getTaxRequest->DocType = DocumentType::SalesOrder;
-	/* Change the DocumentType to SalesInvoice to save this document to the Amdin Console*/
+	/* Change the DocumentType to SalesInvoice to save this document to the Admin Console*/
 
 	getTaxRequest->DocDate = DateTime::Now;
-	/* This will create a Document Date of SAMPLE with date and time appended to it.*/
-
+	
 	getTaxRequest->Commit = false;	//set to true to commit document to reports
 	// ***Commit may also be done separately with PostTax() or CommitTax() methods - see SDK Best Practices.
     
@@ -79,21 +78,21 @@ int main(array<System::String ^> ^args)
 	// The sample uses a static two-line example - you will want to consider how to build a loop that will
 	// allow any number of lines.  Also, please refer to the .NET SDK adapter documentation for other
 	// line variables - we have only used the minimum here.
-    lItem->No = "0001";
-    lItem->ItemCode = "SDK Documentation";
-    lItem->Description = "A box of SDK Documentation";
+	lItem->No = "0001";
+	lItem->ItemCode = "SDK Documentation";
+	lItem->Description = "A box of SDK Documentation";
 	lItem->Qty = Convert::ToDouble(1);
 	lItem->Amount = Convert::ToDecimal(100);
-    getTaxRequest->Lines->Add(lItem);
+	getTaxRequest->Lines->Add(lItem);
 
 	lItem = gcnew Line();
 	lItem->No = "0002";
-    lItem->ItemCode = "Shipping";
-    lItem->Description = "Shipping Item";
+	lItem->ItemCode = "Shipping";
+	lItem->Description = "Shipping Item";
 	lItem->TaxCode = "FR020100";
 	lItem->Qty = Convert::ToDouble(1);
 	lItem->Amount = Convert::ToDecimal(10);
-    getTaxRequest->Lines->Add(lItem);
+	getTaxRequest->Lines->Add(lItem);
 
 	try
 	{
@@ -115,40 +114,38 @@ int main(array<System::String ^> ^args)
 			Console::WriteLine(getTaxResult->Messages[i]->Details);
 		}
 	}
-    else
-    {
+	else
+	{
 		Console::WriteLine("GetTax For Invoice #" + getTaxRequest->DocCode+ " Succeeded");                
 		Console::WriteLine("Total Tax: " + getTaxResult->TotalTax.ToString());
-        Console::WriteLine();
+		Console::WriteLine();
         
 		if (getTaxRequest->DetailLevel > DetailLevel::Document)
-        {
-            Console::WriteLine("Line Taxes:");
-        }
-        for(int tli = 0; tli < getTaxResult->TaxLines->Count; tli++)              
-        {
-            TaxLine ^taxLine = getTaxResult->TaxLines[tli];
-            Console::WriteLine("     Line " + taxLine->No + " Total Taxable: " + taxLine->Taxable.ToString());                    
-            Console::WriteLine("     Line " + taxLine->No + " Total Tax: " + taxLine->Tax.ToString());
+		{
+			Console::WriteLine("Line Taxes:");
+		}
+		for(int tli = 0; tli < getTaxResult->TaxLines->Count; tli++)              
+		{
+			TaxLine ^taxLine = getTaxResult->TaxLines[tli];
+			Console::WriteLine("     Line " + taxLine->No + " Total Taxable: " + taxLine->Taxable.ToString());
+			Console::WriteLine("     Line " + taxLine->No + " Total Tax: " + taxLine->Tax.ToString());
 
 			
 			for(int tdi = 0; tdi < taxLine->TaxDetails->Count; tdi++)
-            {	
-				TaxDetail ^taxDetail = taxLine->TaxDetails->default[tdi];			
-                Console::WriteLine("=============================");
+			{	
+				TaxDetail ^taxDetail = taxLine->TaxDetails->default[tdi];
+				Console::WriteLine("=============================");
 				Console::WriteLine("          Jurisdiction Name: " + taxDetail->JurisName->ToString());
-                Console::WriteLine("          Jurisdiction Tax Name: " + taxDetail->TaxName->ToString());
-                Console::WriteLine("          Jurisdiction Type: " + taxDetail->JurisType.ToString());
+				Console::WriteLine("          Jurisdiction Tax Name: " + taxDetail->TaxName->ToString());
+				Console::WriteLine("          Jurisdiction Type: " + taxDetail->JurisType.ToString());
 				Console::WriteLine("          Jurisdiction Rate: " + taxDetail->Rate.ToString());
 				Console::WriteLine("          Jurisdiction NonTaxable: " + taxDetail->NonTaxable.ToString());
 				Console::WriteLine("          Jurisdiction Taxable: " + taxDetail->Taxable.ToString());
 				Console::WriteLine("          Jurisdiction Tax: " + taxDetail->Tax.ToString());
 				Console::WriteLine("          Jurisdiction Code: " + taxDetail->JurisCode->ToString());
-             
-            }//end for
+			}//end for
 			Console::WriteLine("=============================");
-            
-        }//end for
+		}//end for
 		//to display diagnostic details in the console
 		for(int i = 0; i < getTaxResult->Messages->Count; i++)
 		{
@@ -156,9 +153,7 @@ int main(array<System::String ^> ^args)
 			Console::WriteLine(getTaxResult->Messages[i]->Summary);
 			Console::WriteLine(getTaxResult->Messages[i]->Details);
 		}
-
-       
-    }//end else
+	}//end else
 
 	return 0;
 }
